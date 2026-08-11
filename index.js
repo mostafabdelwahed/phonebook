5edb3233e5eb211df80e2cfdbe751764cd8b8ccf
Mostafa Abdelwahed <mostafabahaa258@gmail.com>
2026-08-11 16:32:13 +0300

    Removed CORS library

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
